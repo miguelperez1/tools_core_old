e61542cba666b634b9144fbcc15b53aa9e862db0
Miguel Perez <36670243+miguelperez1@users.noreply.github.com>
2021-04-25 19:41:08 -0500

    = fixed startup script

--- python/maya_core/maya_startup/__init__.py
+++ python/maya_core/maya_startup/__init__.py
@@ -8,12 +8,21 @@ log = logger.Logger()
 
 def publish_startup_script():
     target_py_path = "C:\\Users\\Miguel\\Documents\\maya\\scripts\\userSetup.py"
+    target_mel_path = "C:\\Users\\Miguel\\Documents\\maya\\scripts\\userSetup.mel"
     src_py_path = "F:\\share\\tools\\tools_core\\python\\maya_core\\maya_startup\\userSetup.py"
+    src_mel_path = "F:\\share\\tools\\tools_core\\python\\maya_core\\maya_startup\\userSetup.mel"
 
     if os.path.isfile(target_py_path) and os.path.isfile(src_py_path):
         os.remove(target_py_path)
 
+    if os.path.isfile(target_mel_path) and os.path.isfile(src_mel_path):
+        os.remove(target_mel_path)
+
     copyfile(src_py_path, target_py_path)
+    # copyfile(src_mel_path, target_mel_path)
+
+    if os.path.isfile(target_mel_path):
+        log.result("Copied userSetup.mel successfully")
 
     if os.path.isfile(target_py_path):
         log.result("Copied userSetup.py successfully")
--- python/maya_core/maya_startup/userSetup.py
+++ python/maya_core/maya_startup/userSetup.py
@@ -1,4 +1,5 @@
 import maya.cmds as cmds
+import maya.mel as mel
 
 import maya_core
 from maya_core.common_tools import logger
@@ -7,24 +8,32 @@ log = logger.Logger()
 
 
 def set_render_settings():
-    cmds.setAttr("vraySettings.aspectLock", 0)
-
-    cmds.setAttr("vraySettings.width", 1920)
-    cmds.setAttr("vraySettings.height", 804)
-
-    cmds.setAttr("vraySettings.aspectRatio", 2.388)
-    cmds.setAttr("vraySettings.pixelAspect", 1.000)
-    cmds.setAttr("vraySettings.clearRVOn", 1)
-
-
-def startup_maya():
-    log.result("loaded tools_core-" + maya_core.version)
+    cmds.setAttr("defaultResolution.width", 1920)
+    cmds.setAttr("defaultResolution.height", 804)
+    cmds.setAttr("defaultResolution.lockDeviceAspectRatio", 0)
+    cmds.setAttr("defaultResolution.deviceAspectRatio", (1920 / 804))
+    cmds.setAttr("defaultResolution.pixelAspect", 1.0)
 
     cmds.setAttr("defaultRenderGlobals.currentRenderer", "vray", type="string")
     log.result("Set V-Ray as current renderer")
 
-    cmds.objExists("vraySettings")
+    mel.eval("vrayCreateVRaySettingsNode();")
+
+    cmds.setAttr("vraySettings.aspectLock", 0)
+    cmds.setAttr("vraySettings.width", 1920)
+    cmds.setAttr("vraySettings.height", 804)
+    cmds.setAttr("vraySettings.aspectRatio", 1920/804)
+    cmds.setAttr("vraySettings.pixelAspect", 1)
 
 
-cmds.evalDeferred(startup_maya, lp=1)
-cmds.evalDeferred(set_render_settings, lp=1)
+def startup_maya():
+    log.result("loaded tools_core-" + maya_core.version)
+    set_render_settings()
+
+
+def main():
+    cmds.evalDeferred(startup_maya, lp=1)
+
+
+if __name__ == "__main__" or __name__ == "maya_core.maya_startup.userSetup":
+    main()
